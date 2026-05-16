ab4e0bc8a0f63ba797455cd66709654170da2b44
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-16 16:40:00 +0900

    Add Supabase configuration

---
+++ Campfire/Config.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+enum SupabaseConfig {
+    static let url = URL(string: "https://azvgjuxedcdqyeixodur.supabase.co")!
+    static let anonKey = "sb_publishable_vCQBY39YfFFf9V-ncMYKzg_U-Fia3Pg"
+}
