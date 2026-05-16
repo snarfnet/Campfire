e86eb55712d1fa4497a1cc0506c3d16893b52b77
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-16 16:41:44 +0900

    Add EndView

---
+++ Campfire/Views/EndView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct EndView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            BonfireView()
+            
+            VStack {
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    Text("またいつか")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 1.0, green: 0.84, blue: 0.71))
+                    
+                    Button(action: { dismiss() }) {
+                        Text("ホームへ戻る")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(red: 1.0, green: 0.7, blue: 0.3))
+                            .cornerRadius(8)
+                    }
+                }
+                .padding(24)
+                .background(Color.black.opacity(0.4))
+                .cornerRadius(12)
+                .padding()
+                
+                Spacer()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
