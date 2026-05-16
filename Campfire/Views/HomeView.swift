be2dd37dcf352f7e2ade8ddf76b70e1d62c9be35
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-16 16:41:26 +0900

    Add HomeView

---
+++ Campfire/Views/HomeView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct HomeView: View {
+    @ObservedObject var authManager: AuthManager
+    @ObservedObject var matchmakingVM: MatchmakingViewModel
+    let user: User
+    
+    var body: some View {
+        ZStack {
+            BonfireView()
+            
+            VStack {
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    Text("焚き火")
+                        .font(.system(size: 40, weight: .bold, design: .default))
+                        .foregroundColor(Color(red: 1.0, green: 0.84, blue: 0.71))
+                    
+                    Button(action: {
+                        matchmakingVM.enterQueue(userId: user.id)
+                    }) {
+                        Text("待つ")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 16)
+                            .background(Color(red: 1.0, green: 0.7, blue: 0.3))
+                            .cornerRadius(12)
+                    }
+                    
+                    if let error = matchmakingVM.error {
+                        Text(error)
+                            .font(.caption)
+                            .foregroundColor(.red)
+                    }
+                }
+                .padding(24)
+                .background(Color.black.opacity(0.3))
+                .cornerRadius(12)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
